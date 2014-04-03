6046d371fe0708e8f2ff6157e820305f2bc51df2
Michael Smith <mdsmtp@gmail.com>
2014-04-02 20:56:57 -0700

    Thin out TriggerReference: eliminate .not()
    Use the TriggerNot class instead. Don't give References any special
    privileges over any other TriggerSource, or you'll run into consistency
    issues.

--- trigger/TriggerReference.java
+++ trigger/TriggerReference.java
@@ -1,26 +1,16 @@
 package com._604robotics.robotnik.trigger;
 
 import com._604robotics.robotnik.network.Slice;
-import com._604robotics.robotnik.prefabs.trigger.TriggerNot;
 
 public class TriggerReference implements TriggerSource {
     private final Trigger trigger;
     private final Slice slice;
     
     private boolean lastValue = false;
-    private TriggerSource inverse = null;
     
     protected TriggerReference (Trigger trigger, Slice slice) {
         this.trigger = trigger;
         this.slice = slice;
-    }
-    
-    public TriggerSource not () {
-        if (inverse == null) {
-            inverse = new TriggerNot(this);
-        }
-        
-        return inverse;
     }
     
     public boolean get () {
