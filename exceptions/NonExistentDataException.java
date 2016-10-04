dc35d785b52b4212e88789b845b7d809403c4402
rlee287 <rlee287@yahoo.com>
2016-10-03 22:42:26 -0700

    Create NonExistentDataException to be thrown for missing key in DataMap

---
+++ exceptions/NonExistentDataException.java
@@ -0,0 +1,24 @@
+
+public class NonExistentDataException extends Exception {
+
+	private static final long serialVersionUID = 121078116205737454L;
+
+	public NonExistentDataException() {}
+
+	public NonExistentDataException(String arg0) {
+		super(arg0);
+	}
+
+	public NonExistentDataException(Throwable arg0) {
+		super(arg0);
+	}
+
+	public NonExistentDataException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public NonExistentDataException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+	}
+
+}
